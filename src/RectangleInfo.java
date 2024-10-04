37fc98b4d9f183a4b9729fbd5944944c2365fbe6
adam <adam@gomaafam.com>
2024-10-04 11:08:55 -0400

    Rectangle Info

---
+++ src/RectangleInfo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RectangleInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double width = 0;
+        double height = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.print("Enter the width rectangle: ");
+            width = Double.parseDouble(scanner.nextLine());
+            System.out.print("Enter the height of the rectangle: ");
+            height = Double.parseDouble(scanner.nextLine());
+            if(width > 0 & height > 0) {
+                validInput = true;
+            }
+        }
+    }
+}
