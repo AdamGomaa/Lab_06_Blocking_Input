344471c6ecfb66285bdff547066bb51c65abcf9e
adam <adam@gomaafam.com>
2024-10-04 11:10:47 -0400

    Rectangle Info

--- src/RectangleInfoCalc.java
+++ src/RectangleInfoCalc.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class RectangleInfo {
+public class RectangleInfoCalc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double width = 0;
